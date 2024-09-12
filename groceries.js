733556389a05f64842049b0ed96019e938a1cabe
Nick <93751758+kofflernick@users.noreply.github.com>
2024-09-12 02:41:49 -0400

    integrated all needed functionality

---
+++ groceries.js
@@ -0,0 +1,26 @@
+const eggs = {
+  name: "eggs",
+  quantity: 0,
+  price: 2.99,
+  bought: false,
+}
+
+const milk = {
+  name: "milk",
+  quantity: 0,
+  price: 1.49,
+  bought: false,
+}
+
+const bread = {
+  name: "bread",
+  quantity: 0,
+  price: 2.49,
+  bought: false,
+}
+
+module.exports = {
+  bread,
+  milk,
+  eggs,
+}
--- index.js
+++ index.js
@@ -1,19 +1,66 @@
-// Import the readline module for handling user input in the console
+const { eggs, milk, bread } = require("./groceries")
+
 const readline = require("readline")
 const rl = readline.createInterface({
   input: process.stdin, // Read from standard input (keyboard)
   output: process.stdout, // Write to standard output (console)
 })
 
-rl.on("line", (line) => {
-  console.log(line)
+const groceryList = []
+const availableItems = { milk, bread, eggs }
+
+function askForItem() {
+  rl.question(
+    "enter item name, 'buy' to begin purchases, or 'exit' to quit: ",
+    (response) => {
+      if (response.toLowerCase() === "exit") {
+        rl.close()
+        return
+      } else if (response.toLowerCase() === "buy") {
+        buyItems()
+        function buyItems() {
+          rl.question(
+            "which item have you bought? type 'exit' to finish: ",
+            (response) => {
+              const item = availableItems[response]
+
+              if (item) {
+                item.bought = true
+                console.log(`item bought: ${item.name}`)
+                console.log(`current list:`, groceryList)
+                buyItems()
+              } else if (response.toLowerCase() === "exit") {
+                rl.close()
+                return
+              } else {
+                console.log("item not in list")
+                buyItems()
+              }
+            }
+          )
+        }
+      }
+
+      const item = availableItems[response]
+
+      if (item) {
+        rl.question("enter the quantity: ", (response) => {
+          item.quantity = response
+          groceryList.push(item)
+          console.log(`added item: ${item.name}`)
+          console.log(`current list:`, groceryList)
+          askForItem()
+        })
+      } else {
+        askForItem()
+      }
+    }
+  )
+}
+
+rl.on("close", () => {
+  console.log("goodbye")
+  console.log(`final list:`, groceryList)
 })
 
-rl.once("close", () => {
-  // end of input
-  console.log("Goodbye")
-})
-
-rl.question("Enter name:", (name) => {
-  console.log(name)
-})
+askForItem()
